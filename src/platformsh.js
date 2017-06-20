26b57e589ea8d0bb85b04dcde6bd94d184829d5d
Gytis Alčiauskas <alciauskas@gmail.com>
2017-06-20 15:07:54 +0300

    Reduced version.

--- src/platformsh.js
+++ src/platformsh.js
@@ -1,64 +1,36 @@
-let jsonConfig;
+// Decode Base64 string and parse JSON.
+const readBase64Json = (value) => {
+  if (!value) {
+    return undefined;
+  }
 
-try{
-  jsonConfig = require('/run/config.json');
-} catch(err) {
-  // Mocking data in dev mode
-  jsonConfig = {
-    info: {
-      limits: {
-        cpu: 1
-      }
-    }
-  };
-}
-/**
-* Read number of CPUs from environment or fallback to the _private_ configuration property
-* Useful for determining the number of processes to fork.
-*/
-function num_of_cpus() {
   try {
-    if(process.env['OMP_NUM_THREADS']) {
-      return process.env['OMP_NUM_THREADS'];
-    }
-
-    return Math.ceil(jsonConfig.info.limits.cpu);
+    return JSON.parse(new Buffer(value, 'base64').toString());
   } catch (err) {
-    throw new Error('Could not get number of cpus');
+    return undefined;
   }
 };
 
-function read_base64_json(varName) {
-  try {
-    return JSON.parse(new Buffer(process.env[varName], 'base64').toString());
-  } catch (err) {
-    throw new Error(`no ${varName} environment variable`);
-  }
-};
-
-/**
-* Reads Platform.sh configuration from environment and returns a single object
-*/
-function config() {
-  if(!process.env.PLATFORM_PROJECT) {
-    throw Error('This is not running on platform.sh');
+// Reads Platform.sh configuration from environment and returns a single object.
+const config = () => {
+  if (!process.env.PLATFORM_PROJECT) {
+    return false;
   }
 
   return {
-    application: read_base64_json('PLATFORM_APPLICATION'),
-    relationships: read_base64_json('PLATFORM_RELATIONSHIPS'),
-    variables: read_base64_json('PLATFORM_VARIABLES'),
+    application: readBase64Json(process.env.PLATFORM_APPLICATION),
+    relationships: readBase64Json(process.env.PLATFORM_RELATIONSHIPS),
+    variables: readBase64Json(process.env.PLATFORM_VARIABLES),
     application_name: process.env.PLATFORM_APPLICATION_NAME,
     app_dir: process.env.PLATFORM_APP_DIR,
     environment: process.env.PLATFORM_ENVIRONMENT,
     project: process.env.PLATFORM_PROJECT,
-    routes: read_base64_json('PLATFORM_ROUTES'),
+    routes: readBase64Json(process.env.PLATFORM_ROUTES),
     tree_id: process.env.PLATFORM_TREE_ID,
     project_entropy: process.env.PLATFORM_PROJECT_ENTROPY,
     branch: process.env.PLATFORM_BRANCH,
     document_root: process.env.PLATFORM_DOCUMENT_ROOT,
-    port: process.env.PORT,
-    omp_num_threads: num_of_cpus()
+    port: process.env.PORT
   };
 };
 
